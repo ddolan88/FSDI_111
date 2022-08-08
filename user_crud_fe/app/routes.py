099238054c4dc7370d249100c85f0f5dfe62980a
ddolan88 <104475059+ddolan88@users.noreply.github.com>
2022-08-08 14:00:55 -0700

    Add files via upload

---
+++ user_crud_fe/app/routes.py
@@ -0,0 +1,22 @@
+from flask import (
+    Flask,
+    render_template
+)
+
+import requests
+
+URL = 'http://127.0.0.1:5000'
+
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def get_index():
+    return render_template('index.html')
+
+
+@app.get("/users")
+def display_users():
+    user_list = requests.get(URL)
+    render_template('users.html', users=user_list)
